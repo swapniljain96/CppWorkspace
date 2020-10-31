930031024a7be36eb4d2f513f88c857e2f294bb4
swapniljain96 <swapnil212160@gmail.com>
2020-10-31 14:44:26 +0530

    commit

--- main.cpp
+++ main.cpp
@@ -1,121 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <stdbool.h>
-#include<limits.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-struct List
-{
-  int i;
-  float w;
-  struct List *next;
-};
-struct EdgeList
-{
-  int i, j;
-  float w;
-  struct EdgeList *next;
-};
-
-struct EdgeList *
-newedgelist (struct EdgeList *node, int i, int j, float w)
-{
-  struct EdgeList *temp =
-    (struct EdgeList *) malloc (sizeof (struct EdgeList));
-  temp->i = i;
-  temp->j = j;
-  temp->w = w;
-  temp->next = node;
-  return temp;
-}
-
-struct List *
-newedge (struct List *node, int i, float w)
-{
-  struct List *temp = (struct List *) malloc (sizeof (struct List));
-  temp->i = i;
-  temp->w = w;
-  temp->next = node;
-  return temp;
-}
-
-void
-BellmanFord (struct EdgeList *elist, float SD[], int s, int n)
-{
-  struct EdgeList *temp;
-  int i;
-
-  for (i = 0; i < n; ++i)
-    SD[i] = INT_MAX;
-  SD[s] = 0;
-
-  for (i = 0; i < n; ++i)
-    {
-      temp = elist;
-      while (temp)
-	{
-	  if (SD[temp->j] > SD[temp->i] + temp->w)
-	    SD[temp->j] = SD[temp->i] + temp->w;
-
-	  temp = temp->next;
-	}
-
-    }
-}
-
-void
-FloydWarshall (float A[][1000], float D[][1000], int n)
-{
-  int i, j, k;
-  for (i = 0; i < n; ++i)
-    for (j = 0; j < n; ++j)
-      D[i][j] = A[i][j];
-  for (i = 0; i < n; ++i)
-    D[i][i] = 0;
-  for (k = 0; k < n; ++k)
-    for (i = 0; i < n; ++i)
-      for (j = 0; j < n; ++j)
-	if (D[i][j] > D[i][k] + D[k][j])
-	  D[i][j] = D[i][k] + D[k][j];
-}
-
-void
-main ()
-{
-  int i, n = 200, l, r, m = 0, j, k;
-  float A[1000][1000], D[1000][1000], SD[1000];
-  struct List *list[n], *temp;
-  struct EdgeList *elist = NULL, *etemp;
-
-  for (i = 0; i < n; ++i)
-    for (j = 0; j < n; ++j)
-      A[i][j] = INT_MAX;
-  for (i = 0; i < n; ++i)
-    list[i] = NULL;
-  for (i = 0; i < n; ++i)
-    for (k = 0; k < n / 10; ++k)
-      {
-	j = rand () % n;
-
-	A[i][j] = (i + j) * 2.1234;
-	list[i] = newedge (list[i], j, (i + j) * 2.1234);
-	elist = newedgelist (elist, i, j, (i + j) * 2.1234);
-	++m;
-
-      }
-
-  printf ("%d\n", m);
-
-  FloydWarshall (A, D, n);
-
-  for (i = 0; i < n; ++i)
-    {
-      BellmanFord (elist, SD, i, n);
-
-      for (j = 0; j < n; ++j)
-
-	if (abs (SD[j] - D[i][j]) > 0.1)
-	  printf ("%f %f \n", SD[j], D[i][j]);
-    }
-
-}
-
+int min(int x, int y) 
+{ return x < y ? x : y; } 
+    
+int max(int x, int y) 
+{ return x > y ? x : y; } 
+    
+long long int maxSubProduct(int a[],int n) 
+    {  long long int maxeh = 1,mineh = 1,msf = 1,flag = 0; 
+       for (int i = 0; i < n; i++) { 
+            if (a[i] > 0) { 
+                maxeh = maxeh * a[i]; 
+                mineh = min(mineh * a[i], 1); 
+                flag = 1; 
+            } 
+             else if (a[i] == 0) { 
+                maxeh = 1; 
+                mineh = 1;  } 
+            else { 
+                long long int temp = maxeh; 
+                maxeh = max(mineh * a[i], 1); 
+                mineh = temp * a[i]; } 
+            if (msf < maxeh) 
+                msf = maxeh; 
+        } 
+        if (flag == 0 && msf == 1) 
+            return 0; 
+        return msf; 
+    } 
+  
+int main() 
+{  int a[] = { 9,5,3,-4,8,-6,10,-11,52,-20,4,-30,89}; 
+    int n= sizeof(a) / sizeof(a[0]); ;
+    cout<<"\n\nMaximum Subarray product is: "<<maxSubProduct(a,n); 
+    return 0;
+} 
