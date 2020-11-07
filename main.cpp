ac2a99c966bec43f69450313bd8fe1303bde6b77
swapniljain96 <swapnil212160@gmail.com>
2020-11-07 23:43:26 +0530

    main

--- main.cpp
+++ main.cpp
@@ -7,13 +7,6 @@ using namespace std;
 
 int main() 
 { 
- string myString = "10 15 20 23";
- stringstream iss( myString );
- int number;
- std::vector<int> myNumbers;
- while ( iss >> number )
-  {  myNumbers.push_back( number ); }
-   
     cout<<"\n";
     systyem("pause");
     return 0;
