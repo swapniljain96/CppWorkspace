90934926ad52593dccdfaeeb70273a3dd1376fa1
Swapnil Jain <swapnil212160@gmail.com>
2020-11-17 12:14:02 +0530

    Coding

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main() 
-{ 
-    cout<<"This is DELL G3 \n";
+{   int a[] = { };
+    int n = sizeof(a) / sizeof(a[0]);
+    string s;
+    
+    cout<<"\nThis is DELL G3 \n";
     system("pause");
     return 0;
 } 
