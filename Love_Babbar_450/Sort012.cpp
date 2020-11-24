720d0eb38e352e4d498c257e69f592e012c4625b
Swapnil Jain <swapnil212160@gmail.com>
2020-11-24 11:52:58 +0530

    edit

--- Love_Babbar_450/Sort012.cpp
+++ Love_Babbar_450/Sort012.cpp
@@ -2,11 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 void sort012(int[],int);
-
 int main() {
     int t;
     cin >> t;
-
     while(t--){
         int n;
         cin >>n;
@@ -14,22 +12,16 @@ int main() {
         for(int i=0;i<n;i++){
             cin >> a[i];
         }
-
         sort012(a, n);
-
         for(int i=0;i<n;i++){
             cout << a[i]  << " ";
         }
-
         cout << endl;
-        
-        
     }
     return 0;
 }
 
 // } Driver Code Ends
-
 void sort012(int a[], int n)
 {  int n0=0,n1=0,n2=0;
     for(int i=0;i<n;i++)    
